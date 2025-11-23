d4bccf29563adf2966864cbc9f579780ee9fbd6e
GH-Tyron <110431714+GH-Tyron@users.noreply.github.com>
2025-11-23 23:45:45 +0000

    Update Button.tsx

--- alx-project-0x03/components/common/Button.tsx
+++ alx-project-0x03/components/common/Button.tsx
@@ -1,16 +1,28 @@
+## components/common/Button.tsx
+
 interface ButtonProps {
-    label: string;
-    onClick?: () => void;
-    className: string;
+  buttonLabel: string
+  buttonSize?: string
+  buttonBackgroundColor?: 'red' | 'blue' | 'orange' | 'green'
+  action?: () => void
 }
 
-export default function Button ({label, onClick, className}: ButtonProps) {
-    return(
-        <button className={"p-4 py-2 bg-blue-600 text-white rounded ${className}"}
-        onClick={onClick}
-        >
-        {label}
-        </button>
-    )
-    
+
+const Button = ({ buttonLabel, buttonSize, buttonBackgroundColor, action }: ButtonProps) => {
+
+  const backgroundColorClass = buttonBackgroundColor ? {
+    red: 'bg-red-500',
+    blue: 'bg-blue-500',
+    orange: 'bg-orange-500',
+    green: 'bg-green-500',
+  }[buttonBackgroundColor] : 'bg-slate-500'
+
+
+  return (
+    <button onClick={action} className={`${backgroundColorClass} ${buttonSize} px-6 py-2 text-sm font-semibold rounded-lg hover:${backgroundColorClass}/50 transition duration-300 text-white`}>
+      {buttonLabel}
+    </button>
+  )
 }
+
+export default Button;
